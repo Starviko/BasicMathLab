a754492faf441a5917d49ff26aad7a36babb1c9e
StarWalker-v <pengjingguo2022@gmail.com>
2023-03-23 20:54:56 -0600

    Add project files.

---
+++ BasicMath.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMathLab
+{
+    public class BasicMath
+    {
+        public double AddMethod(double a,double b)
+        {
+            double result = a + b;
+            return result;
+        }
+        public double SubstractMethod(double a,double b)
+        {
+            double result = a - b;
+            return result;
+        }
+        public double MultiplyMethod(double a , double b)
+        {
+            double result = a * b;
+            return result;
+        }
+        public double DivideMethod(double a,double b)
+        {
+            double result = a / b;
+            return result;
+        }
+
+    }
+}
